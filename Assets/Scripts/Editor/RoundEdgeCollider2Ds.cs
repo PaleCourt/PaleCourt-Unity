61ec0e58fe6b0fd0d26a1f4505ea0f3f07023add
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2023-06-16 18:00:48 +0200

    add Parkour Scene add editor scripts for easier editing

---
+++ Assets/Scripts/Editor/RoundEdgeCollider2Ds.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public static class RoundEdgeCollider2Ds
+{
+    [MenuItem("CONTEXT/EdgeCollider2D/Round Vertices")]
+    public static void RoundEdgeCollider2D(MenuCommand menuCommand) {
+        EdgeCollider2D ec2d = menuCommand.context as EdgeCollider2D;
+        var points = ec2d.points;
+        for (int i = 0; i < points.Length; i++) {
+            points[i].x = Mathf.Round (points[i].x);
+            points[i].y = Mathf.Round (points[i].y);
+        }
+        ec2d.points = points;
+    }
+    [MenuItem("CONTEXT/PolygonCollider2D/Round Vertices")]
+    public static void RoundPolygonCollider2D(MenuCommand menuCommand) {
+        PolygonCollider2D pc2d = menuCommand.context as PolygonCollider2D;
+        var points = pc2d.points;
+        for (int i = 0; i < points.Length; i++) {
+            points[i].x = Mathf.Round (points[i].x);
+            points[i].y = Mathf.Round (points[i].y);
+        }
+        pc2d.points = points;
+    }
+}
