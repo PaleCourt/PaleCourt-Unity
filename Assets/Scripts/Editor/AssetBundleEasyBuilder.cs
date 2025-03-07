15a0e82aacce2b309c14b6a93f443377b99b29f7
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2025-03-07 17:50:05 +0100

    adjust build script

--- Assets/Scripts/Editor/AssetBundleEasyBuilder.cs
+++ Assets/Scripts/Editor/AssetBundleEasyBuilder.cs
@@ -3,34 +3,32 @@ using System.IO;
 
 public class CreateAssetBundles
 {
+    static void BuildAssetBundlesCompressed(string folderName, BuildTarget target)
+    {
+        string assetBundleDirectory = $"AssetBundles/Standalone{folderName}";
+        if(!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, target);
+    }
+    static void BuildStandaloneWindows64()
+    {
+        BuildAssetBundlesCompressed("Windows", BuildTarget.StandaloneWindows64);
+    }
+    static void BuildStandaloneOSX()
+    {
+        BuildAssetBundlesCompressed("OSX", BuildTarget.StandaloneOSX);
+    }
+    static void BuildStandaloneLinux64()
+    {
+        BuildAssetBundlesCompressed("Linux", BuildTarget.StandaloneLinux64);
+    }
     [MenuItem("Build AssetBundles/Build Bundles")]
     static void BuildAllAssetBundlesCompressed()
     {
-        string assetBundleDirectory = "AssetBundles/StandaloneWindows";
-        if(!Directory.Exists(assetBundleDirectory))
-        {
-            Directory.CreateDirectory(assetBundleDirectory);
-        }
-        BuildPipeline.BuildAssetBundles(assetBundleDirectory,
-                                        BuildAssetBundleOptions.None,
-                                        BuildTarget.StandaloneWindows64);
-
-        assetBundleDirectory = "AssetBundles/StandaloneOSX";
-        if(!Directory.Exists(assetBundleDirectory))
-        {
-            Directory.CreateDirectory(assetBundleDirectory);
-        }
-        BuildPipeline.BuildAssetBundles(assetBundleDirectory,
-                                        BuildAssetBundleOptions.None,
-                                        BuildTarget.StandaloneOSX);
-
-        assetBundleDirectory = "AssetBundles/StandaloneLinux";
-        if(!Directory.Exists(assetBundleDirectory))
-        {
-            Directory.CreateDirectory(assetBundleDirectory);
-        }
-        BuildPipeline.BuildAssetBundles(assetBundleDirectory,
-                                        BuildAssetBundleOptions.None,
-                                        BuildTarget.StandaloneLinux64);
+        BuildStandaloneWindows64();
+        BuildStandaloneOSX();
+        BuildStandaloneLinux64();
     }
 }
